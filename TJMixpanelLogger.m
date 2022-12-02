392aa5efa833348619358a1fdac1aa14bb4c873f
Tim Johnsen <timonus@gmail.com>
2022-12-01 21:04:00 -0800

    Move work to background and take background exception while it's occurring.

--- TJMixpanelLogger.m
+++ TJMixpanelLogger.m
@@ -62,128 +62,139 @@ static NSString *_uuidToBase64(NSUUID *const uuid)
         NSLog(@"Invalid %s", __PRETTY_FUNCTION__);
         return;
     }
-
-    static NSDictionary *staticProperties;
-    static NSMutableURLRequest *staticRequest;
-    static NSURLSession *session;
-    static dispatch_once_t onceToken;
-    dispatch_once(&onceToken, ^{
-        _sessionConfiguration = [NSURLSessionConfiguration backgroundSessionConfigurationWithIdentifier:[NSString stringWithFormat:@"com.tijo.logger.%@", [[NSUUID UUID] UUIDString]]];
-        _sessionConfiguration.sharedContainerIdentifier = _sharedContainerIdentifier;
-        _sessionConfiguration.sessionSendsLaunchEvents = NO;
-        _sessionConfiguration.networkServiceType = NSURLNetworkServiceTypeBackground;
-        _sessionConfiguration.discretionary = YES;
-        session = [NSURLSession sessionWithConfiguration:_sessionConfiguration];
-
-        const CGFloat width = [UIScreen mainScreen].bounds.size.width;
-        const CGFloat height = [UIScreen mainScreen].bounds.size.height;
+    
+    NSString *const insertIdentifier = _uuidToBase64([NSUUID UUID]);
+    
+    // -performExpiringActivityWithReason:... has two benefits
+    // (1) It keeps the process alive long enough to finish the work within its block. This is especially useful for extension which might be shortlived.
+    // (2) It runs the work in the block on another thread so we avoid blocking the calling thread.
+    [[NSProcessInfo processInfo] performExpiringActivityWithReason:insertIdentifier usingBlock:^(BOOL expired) {
+        if (expired) {
+            return;
+        }
         
-        NSString *deviceModel = nil;
+        static NSDictionary *staticProperties;
+        static NSMutableURLRequest *staticRequest;
+        static NSURLSession *session;
+        static dispatch_once_t onceToken;
+        dispatch_once(&onceToken, ^{
+            _sessionConfiguration = [NSURLSessionConfiguration backgroundSessionConfigurationWithIdentifier:[NSString stringWithFormat:@"com.tijo.logger.%@", [[NSUUID UUID] UUIDString]]];
+            _sessionConfiguration.sharedContainerIdentifier = _sharedContainerIdentifier;
+            _sessionConfiguration.sessionSendsLaunchEvents = NO;
+            _sessionConfiguration.networkServiceType = NSURLNetworkServiceTypeBackground;
+            _sessionConfiguration.discretionary = YES;
+            session = [NSURLSession sessionWithConfiguration:_sessionConfiguration];
+            
+            const CGFloat width = [UIScreen mainScreen].bounds.size.width;
+            const CGFloat height = [UIScreen mainScreen].bounds.size.height;
+            
+            NSString *deviceModel = nil;
 #if TARGET_IPHONE_SIMULATOR
-        deviceModel = [[[NSProcessInfo processInfo] environment] objectForKey:@"SIMULATOR_MODEL_IDENTIFIER"];
+            deviceModel = [[[NSProcessInfo processInfo] environment] objectForKey:@"SIMULATOR_MODEL_IDENTIFIER"];
 #else
-        struct utsname systemInfo;
-        uname(&systemInfo);
-        deviceModel = [NSString stringWithCString:systemInfo.machine encoding:NSUTF8StringEncoding]; // https://stackoverflow.com/a/11197770/3943258
+            struct utsname systemInfo;
+            uname(&systemInfo);
+            deviceModel = [NSString stringWithCString:systemInfo.machine encoding:NSUTF8StringEncoding]; // https://stackoverflow.com/a/11197770/3943258
 #endif
-        BOOL isOnMac = NO;
-        if (@available(iOS 13.0, *)) {
-            if ([[NSProcessInfo processInfo] isMacCatalystApp]) {
-                isOnMac = YES;
-            } else if (@available(iOS 14.0, *)) {
-                if ([[NSProcessInfo processInfo] isiOSAppOnMac]) {
+            BOOL isOnMac = NO;
+            if (@available(iOS 13.0, *)) {
+                if ([[NSProcessInfo processInfo] isMacCatalystApp]) {
                     isOnMac = YES;
+                } else if (@available(iOS 14.0, *)) {
+                    if ([[NSProcessInfo processInfo] isiOSAppOnMac]) {
+                        isOnMac = YES;
+                    }
                 }
             }
-        }
-        if (isOnMac && deviceModel) {
-            deviceModel = [@"Mac-" stringByAppendingString:deviceModel];
-        }
-        
-        // https://help.mixpanel.com/hc/en-us/articles/115004613766-Default-Properties-Collected-by-Mixpanel#ios
-        // https://github.com/mixpanel/mixpanel-iphone/blob/master/Sources/Mixpanel.m#L510
-        staticProperties = @{
-            @"token": _projectToken,
-            @"distinct_id": _uuidToBase64([[UIDevice currentDevice] identifierForVendor]),
-            @"$app_version_string": [[[NSBundle mainBundle] infoDictionary] objectForKey:@"CFBundleShortVersionString"],
-            @"$os_version": [[UIDevice currentDevice] systemVersion],
-            @"$model": deviceModel,
-            @"$screen_height": @((unsigned long)MAX(width, height)),
-            @"$screen_width": @((unsigned long)MIN(width, height)),
-            // Custom
-            @"language": [[NSLocale preferredLanguages] firstObject]
-        };
-        
-        // Find "suffix" on top of main app bundle ID if this is an extension.
-        NSURL *bundleURL = [[NSBundle mainBundle] bundleURL];
-        // https://stackoverflow.com/a/62619735/3943258
-        for (; ![bundleURL.pathExtension isEqualToString:@"app"]; bundleURL = bundleURL.URLByDeletingLastPathComponent);
-        NSString *mainAppBundleIdentifier;
-        if (bundleURL) {
-             mainAppBundleIdentifier = [[NSBundle bundleWithURL:bundleURL] bundleIdentifier];
-        }
-        NSString *bundleIdentifierSuffix = [[NSBundle mainBundle] bundleIdentifier];
-        if ([bundleIdentifierSuffix hasPrefix:mainAppBundleIdentifier]) {
-            bundleIdentifierSuffix = [bundleIdentifierSuffix substringFromIndex:mainAppBundleIdentifier.length];
-            if ([bundleIdentifierSuffix hasPrefix:@"."]) { // Strip off leading "." as well
-                [bundleIdentifierSuffix substringFromIndex:1];
+            if (isOnMac && deviceModel) {
+                deviceModel = [@"Mac-" stringByAppendingString:deviceModel];
             }
-        }
-        
-        if (bundleIdentifierSuffix.length) {
-            NSMutableDictionary *mutableStaticProperties = [staticProperties mutableCopy];
-            mutableStaticProperties[@"bundle_id_suffix"] = bundleIdentifierSuffix;
-            staticProperties = [mutableStaticProperties copy];
-        }
-        
-        NSURLComponents *const components = [NSURLComponents componentsWithString:@"https://api.mixpanel.com/track"];
-        
-        components.queryItems = @[
-            [NSURLQueryItem queryItemWithName:@"ip" value:@"1"],
+            
+            // https://help.mixpanel.com/hc/en-us/articles/115004613766-Default-Properties-Collected-by-Mixpanel#ios
+            // https://github.com/mixpanel/mixpanel-iphone/blob/master/Sources/Mixpanel.m#L510
+            staticProperties = @{
+                @"token": _projectToken,
+                @"distinct_id": _uuidToBase64([[UIDevice currentDevice] identifierForVendor]),
+                @"$app_version_string": [[[NSBundle mainBundle] infoDictionary] objectForKey:@"CFBundleShortVersionString"],
+                @"$os_version": [[UIDevice currentDevice] systemVersion],
+                @"$model": deviceModel,
+                @"$screen_height": @((unsigned long)MAX(width, height)),
+                @"$screen_width": @((unsigned long)MIN(width, height)),
+                // Custom
+                @"language": [[NSLocale preferredLanguages] firstObject]
+            };
+            
+            // Find "suffix" on top of main app bundle ID if this is an extension.
+            NSURL *bundleURL = [[NSBundle mainBundle] bundleURL];
+            // https://stackoverflow.com/a/62619735/3943258
+            for (; ![bundleURL.pathExtension isEqualToString:@"app"]; bundleURL = bundleURL.URLByDeletingLastPathComponent);
+            NSString *mainAppBundleIdentifier;
+            if (bundleURL) {
+                mainAppBundleIdentifier = [[NSBundle bundleWithURL:bundleURL] bundleIdentifier];
+            }
+            NSString *bundleIdentifierSuffix = [[NSBundle mainBundle] bundleIdentifier];
+            if ([bundleIdentifierSuffix hasPrefix:mainAppBundleIdentifier]) {
+                bundleIdentifierSuffix = [bundleIdentifierSuffix substringFromIndex:mainAppBundleIdentifier.length];
+                if ([bundleIdentifierSuffix hasPrefix:@"."]) { // Strip off leading "." as well
+                    [bundleIdentifierSuffix substringFromIndex:1];
+                }
+            }
+            
+            if (bundleIdentifierSuffix.length) {
+                NSMutableDictionary *mutableStaticProperties = [staticProperties mutableCopy];
+                mutableStaticProperties[@"bundle_id_suffix"] = bundleIdentifierSuffix;
+                staticProperties = [mutableStaticProperties copy];
+            }
+            
+            NSURLComponents *const components = [NSURLComponents componentsWithString:@"https://api.mixpanel.com/track"];
+            
+            components.queryItems = @[
+                [NSURLQueryItem queryItemWithName:@"ip" value:@"1"],
 #if DEBUG
-            [NSURLQueryItem queryItemWithName:@"verbose" value:@"1"],
+                [NSURLQueryItem queryItemWithName:@"verbose" value:@"1"],
 #endif
-        ];
+            ];
+            
+            staticRequest = [NSMutableURLRequest requestWithURL:components.URL];
+            [staticRequest setHTTPMethod:@"POST"];
+            [staticRequest setValue:@"application/json" forHTTPHeaderField:@"Content-Type"];
+            staticRequest.cachePolicy = NSURLRequestReloadIgnoringCacheData;
+        });
         
-        staticRequest = [NSMutableURLRequest requestWithURL:components.URL];
-        [staticRequest setHTTPMethod:@"POST"];
-        [staticRequest setValue:@"application/json" forHTTPHeaderField:@"Content-Type"];
-        staticRequest.cachePolicy = NSURLRequestReloadIgnoringCacheData;
-    });
-    
-    // https://developer.mixpanel.com/reference/track-event
-    NSMutableDictionary *const properties = [staticProperties mutableCopy];
-    [properties addEntriesFromDictionary:@{
-        @"time": @((unsigned long)([[NSDate date] timeIntervalSince1970] * 1000)),
-        @"$insert_id": _uuidToBase64([NSUUID UUID]),
-    }];
-    
-    [properties addEntriesFromDictionary:customProperties];
-    
-    NSMutableURLRequest *const request = [staticRequest mutableCopy];
-    NSJSONWritingOptions options = 0;
-    if (@available(iOS 13.0, *)) {
-        options = NSJSONWritingWithoutEscapingSlashes;
-    }
-    [request setHTTPBody:[NSJSONSerialization dataWithJSONObject:@[
-        @{
-            @"event": name,
-            @"properties": properties,
+        // https://developer.mixpanel.com/reference/track-event
+        NSMutableDictionary *const properties = [staticProperties mutableCopy];
+        [properties addEntriesFromDictionary:@{
+            @"time": @((unsigned long)([[NSDate date] timeIntervalSince1970] * 1000)),
+            @"$insert_id": insertIdentifier,
+        }];
+        
+        [properties addEntriesFromDictionary:customProperties];
+        
+        NSMutableURLRequest *const request = [staticRequest mutableCopy];
+        NSJSONWritingOptions options = 0;
+        if (@available(iOS 13.0, *)) {
+            options = NSJSONWritingWithoutEscapingSlashes;
         }
-    ]
-                                                         options:options
-                                                           error:nil]];
-    NSURLSessionTask *task;
+        [request setHTTPBody:[NSJSONSerialization dataWithJSONObject:@[
+            @{
+                @"event": name,
+                @"properties": properties,
+            }
+        ]
+                                                             options:options
+                                                               error:nil]];
+        NSURLSessionTask *task;
 #if DEBUG
-    task = [[NSURLSession sharedSession] dataTaskWithRequest:request completionHandler:^(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error) {
-        NSLog(@"%@ %@ %@", response, error, [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding]);
-    }];
+        task = [[NSURLSession sharedSession] dataTaskWithRequest:request completionHandler:^(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error) {
+            NSLog(@"%@ %@ %@", response, error, [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding]);
+        }];
 #else
-    task = [session downloadTaskWithRequest:request];
-    task.countOfBytesClientExpectsToReceive = 0;
+        task = [session downloadTaskWithRequest:request];
+        task.countOfBytesClientExpectsToReceive = 0;
 #endif
-    task.countOfBytesClientExpectsToSend = request.HTTPBody.length;
-    [task resume];
+        task.countOfBytesClientExpectsToSend = request.HTTPBody.length;
+        [task resume];
+    }];
 }
 
 @end
